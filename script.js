561b507ad1b93ed0bece8e95a6ad20cb3f1b7f9d
m.alrmah <mohamedalrmah05@gmail.com>
2026-05-20 20:29:19 +0200

    commiting beofre battery turns of but trying to make register and login swich

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 //i will be making a function that removes the active class and adds it to to other class that does not have the class
 
-function showForm(formId) {
+function showForm(event, formId) {
+  event.preventDefault(); //prevents page reload
+
   document
     .querySelectorAll(".form-box") // we chose selector all to choose all classes not just the first
-    .forEach((form) => form.classList.remove("active"));
-  document.getElementById(formId).classList.add("active");
+    .forEach((form) => form.classList.remove("active")); //loops through all classes  that have form-box and removes active
+  document.getElementById(formId).classList.add("active"); //only form with matching id will get active added
 }
+
+const registerLink = document.querySelector("#a-register");
+registerLink.addEventListener("click", (event) => {
+  showForm(event, "register");
+});
+
+const loginLink = document.querySelector("a-login");
+loginLink.addEventListener("click", (event) => {
+  showForm(event, "login");
+});
