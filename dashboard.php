1e2c5706d98451fce0e5921df15f8605f734cf21
m.alrmah <mohamedalrmah05@gmail.com>
2026-05-30 16:06:04 +0200

    added header and start of dashboard

---
+++ dashboard.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/dashboard.css">
+    <title>Everest Dashboard</title>
+</head>
+<body>
+    
+<!-- Start of header  -->
+ <div class="header">
+     <div id="logo">Everest</div>
+     <div class="left-links">
+        <ul>
+            <li><a href="#">login</a></li>
+            <li><a href="#">Information</a></li>
+            <li><a href="#">Contact us</a></li>
+        </ul>
+     </div>
+ </div>
+ <!-- End of header  -->
+
+ <!-- start of Dashboar -->
+  <div class="dashboard">
+    <div id=""calendar>
+
+    </div>
+    <div id="day-details"></div>
+
+
+  </div>
+  <!-- End of Dashboard -->
+   
+
+</body>
+</html>
